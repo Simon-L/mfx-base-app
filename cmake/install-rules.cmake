6586472462d29774514c25f7790c5ebd82a2d48a
FergusL <Simon-L@users.noreply.github.com>
2024-05-27 02:34:02 +0200

    Initial commit

---
+++ cmake/install-rules.cmake
@@ -0,0 +1,8 @@
+install(
+    TARGETS mfx-base-app_exe
+    RUNTIME COMPONENT mfx-base-app_Runtime
+)
+
+if(PROJECT_IS_TOP_LEVEL)
+  include(CPack)
+endif()
---
+++ source/lib.c
@@ -0,0 +1,7 @@
+#include "lib.h"
+
+struct library create_library()
+{
+  struct library lib = {"mfx-base-app"};
+  return lib;
+}
