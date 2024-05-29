bc3169c4fb84c89180bf05da6c5584e2bbe7a736
FergusL <Simon-L@users.noreply.github.com>
2024-05-29 16:12:04 +0200

    Remove lib from testing...

--- test/source/mfx-base-app_test.c
+++ test/source/mfx-base-app_test.c
@@ -1,13 +1,7 @@
-#include <string.h>
-
-#include "lib.h"
+#include <stdio.h>
 
 int main(int argc, char const* argv[])
 {
-  struct library lib = create_library();
-
-  (void)argc;
-  (void)argv;
-
-  return strcmp(lib.name, "mfx-base-app") == 0 ? 0 : 1;
+  printf("Hello world!\n");
+  return 0;
 }
