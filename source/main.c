cd8cd5f7e1d68047cea6472469f464118ad2482a
FergusL <Simon-L@users.noreply.github.com>
2024-05-29 15:54:29 +0200

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 
-#include "lib.h"
-
 int main(int argc, char const* argv[])
 {
-  struct library lib = create_library();
-
-  (void)argc;
-  (void)argv;
-
-  if (printf("Hello from %s!", lib.name) < 0) {
-    return 1;
-  }
-
+  printf("Hello world!\n");
   return 0;
 }
